d918751bc6c7d529ca2c0d1d2b46d47e63cdaa8e
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-06-14 22:58:47 +0900

    OAuth2로-로그인로그아웃-구현하기-2

---
+++ src/main/java/com/springboot/dogmeeting/DogMeetingApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.dogmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DogMeetingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DogMeetingApplication.class, args);
+    }
+}
