43ddb768dd0f7706e5261bf26a239bac6718afd2
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-06-14 22:51:31 +0900

    OAuth2로-로그인로그아웃-구현하기-2

---
+++ src/test/java/com/springboot/dogmeeting/DogMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.dogmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
