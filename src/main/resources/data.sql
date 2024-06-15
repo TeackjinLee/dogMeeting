c926d89fd91df46d8363faf11c2deb3d2b56f831
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-06-15 09:34:38 +0900

    OAuth2로-로그인로그아웃-구현하기-2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목1', '내용1', 'user1', NOW(), NOW())
 INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목2', '내용2', 'user2', NOW(), NOW())
 INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목3', '내용3', 'user3', NOW(), NOW())
+INSERT INTO users (EMAIL, PASSWORD) VALUES ('dlxorwls10151@gmail.com',	'$2a$10$VASKFCaDqr9GOOG0jg8pWOpZLiEIVZX5AWC1ptN/xDjc1NoG8U3hS')
