494b1e58dbd5b17a83199f9c9a7f62d26d4827ea
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-06-16 23:02:24 +0900

    Docker로 배포하기

--- src/main/java/com/springboot/dogmeeting/DogMeetingApplication.java
+++ src/main/java/com/springboot/dogmeeting/DogMeetingApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class DogMeetingApplication {
     public static void main(String[] args) {
         SpringApplication.run(DogMeetingApplication.class, args);
